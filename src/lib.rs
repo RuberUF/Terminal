18e49685c68f1089fc5b8ae4ea15b9ba8d1dd2ff
RuberUF <draugr.zip@gmail.com>
2026-06-07 21:14:38 -0500

    rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,25 @@
 use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub fn main(comando:String)-> String{
-    let mut c=String::from("");
-    if comando == "help"{
-        c=help();
-    }else if comando == "cat flag.txt"{
-        c=cat();
-    }else if comando == "ls"{
-        c=ls();
-    }else{
-        c=String::from("N0t f0und l0l");
+    let partes: Vec<&str>=comando.split_whitespace().collect();
+    let c = if partes.len()== 0{
+        String::from("")
     }
+    else{
+        if partes[0] == "help"{
+            help()
+        }else if partes[0] == "cat"{
+            if partes.len() > 1{
+                cat(partes[1])
+            }else {
+                String::from("3rr0r, ejemplo(cat file)")
+            }
+        }else if partes[0] == "ls"{
+            ls()
+        }else{
+            String::from("N0t f0und l0l")
+        }
+    };
     return c;
 }
 fn help()->String{
@@ -19,6 +28,13 @@ fn help()->String{
 fn ls()->String{
     return String::from("flag.txt README.md");
 }
-fn cat()->String{
-    return String::from("CSU{Noc_n0c_n0t_broma}");
+fn cat(c:&str)->String{
+    let d = if c=="flag.txt"{
+        String::from("fuck{Noc_n0c_n0t_broma}")
+    }else if c == "README.md"{
+        String::from("W3lc0m3 :3")
+    }else{
+        String::from("zzzz")
+    };
+    return d;
 }
